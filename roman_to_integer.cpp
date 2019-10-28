eceb33b83a6e2a899d57da4dcfc58c2047dae495
Paolo Torres <ptorres@clearpathrobotics.com>
2019-10-28 17:03:16 -0400

    roman to integer

---
+++ roman_to_integer.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map <char, int> m = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}            
+        };
+        int result = 0;
+        for (auto cur = s.begin(); cur != s.end(); ++cur) {
+            auto nxt = next(cur);
+            if (nxt == s.end()) {
+                result += m[*cur];
+            } else if (*cur == 'I' && (*nxt == 'V' || *nxt == 'X')) {
+                result += m[*nxt] - m[*cur];
+                ++cur;
+            } else if (*cur == 'X' && (*nxt == 'L' || *nxt == 'C')) {
+                result += m[*nxt] - m[*cur];
+                ++cur;
+            } else if (*cur == 'C' && (*nxt == 'D' || *nxt == 'M')) {
+                result += m[*nxt] - m[*cur];
+                ++cur;
+            } else {
+                result += m[*cur];
+            }
+        }
+        return result;
+    }
+};
