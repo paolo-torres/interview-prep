cb7857196ae82ecd39b29f75ef7cc1bb12abf434
Paolo Torres <paolo-torres@hotmail.com>
2019-11-05 11:10:15 -0500

    Create longest_palindrome_substring.cpp

---
+++ longest_palindrome_substring.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if (s.size() == 0 || s.size() == 1) {
+            return true;
+        }
+        int n = s.size();
+        for (int i = 0; i < n / 2; i++) {
+            if (s[i] != s[n-i-1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    string longestPalindrome(string s) {
+        if (s.size() == 0 || s.size() == 1) {
+            return s;
+        }
+        string result;
+        int maxLength = 0;
+        for (int i = 0; i < s.size() - maxLength; i++) {
+            for (int j = s.size() - 1; j >= i + maxLength; j--) {
+                if (s[i] != s[j]) {
+                    continue;
+                }
+                string str = s.substr(i, j - i + 1);
+                if (isPalindrome(str) && str.size() > maxLength) {
+                    maxLength = str.size();
+                    result = str;
+                }
+            }
+        }
+        return result;
+    }
+};
