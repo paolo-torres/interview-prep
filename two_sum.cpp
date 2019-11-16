13eaa6f1d376583a9f4d23aa9a68973714458991
paolotorres <paolo-torres@hotmail.com>
2019-11-16 16:05:44 -0500

    redo two sum

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map <int, int> myMap;
+        unordered_map<int, int> myMap;
         vector<int> result;
-        for (int i = 0; i < nums.size(); ++i) {
-            int numToAdd = target - nums[i];
-            auto it = myMap.find(numToAdd);
-            if (it != myMap.end()) {
-                result.push_back(it->second);
+        for (int i = 0; i < nums.size(); i++) {
+            int value = target - nums[i];
+            auto complement = myMap.find(value);
+            if (complement != myMap.end()) {
+                result.push_back(complement->second);
                 result.push_back(i);
-                return result;
+                break;
+            } else {
+                myMap.insert({nums[i], i});
             }
-            myMap.insert({nums[i], i});
         }
         return result;
     }
