f9e294bb68e4dcc9580ef72ff10c556a539400eb
Paolo Torres <paolo-torres@hotmail.com>
2019-11-16 21:55:16 -0500

    Create subarrays_with_k_different_integers.cpp

---
+++ subarrays_with_k_different_integers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& A, int K) {
+        int result = 0;
+        vector<int> v(A.size() + 1);
+        for (int i = 0, j = 0, count = 0, prefix = 0; i < A.size(); i++) {
+            if (v[A[i]]++ == 0) {
+                count++;
+            }
+            if (count > K) {
+                v[A[j++]]--;
+                count--;
+                prefix = 0;
+            }
+            while (v[A[j]] > 1) {
+                prefix++;
+                v[A[j++]]--;
+            }
+            if (count == K) {
+                result += prefix + 1;
+            }
+        }
+        return result;
+    }
+};
