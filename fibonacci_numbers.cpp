bbff52cf27ad5cfd7150c723fcc812937e783493
Paolo Torres <paolo-torres@hotmail.com>
2019-11-18 16:49:40 -0500

    Create fibonacci_numbers.cpp

---
+++ fibonacci_numbers.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <iostream>
+
+using namespace std;
+
+int fib(int n) {
+    int arr[n+2];
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[n];
+}
+
+int main()
+{
+    for (int i = 0; i < 47; i++) {
+        cout << "index: "<< i << " val: " << fib(i) << endl;
+    }
+    return 0;
+}
