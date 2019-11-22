9059c54b408289952fa9609cc37794957b97a100
Paolo Torres <paolo-torres@hotmail.com>
2019-11-22 12:13:15 -0500

    Update maximum_difference.cpp

--- maximum_difference.cpp
+++ maximum_difference.cpp
@@ -25,8 +25,10 @@ int main()
 {
     vector<int> v1 = {2, 3, 10, 6, 4, 8, 1};
     vector<int> v2 = {7, 9, 5, 6, 3, 2};
+    vector<int> v3 = {5, 10, 7, 6, 4, 3, 1};
     cout << "v1: " << maximumDifference(v1) << endl;
     cout << "v2: " << maximumDifference(v2) << endl;
+    cout << "v3: " << maximumDifference(v3) << endl;
 
     return 0;
 }
