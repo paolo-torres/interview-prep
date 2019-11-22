30f3f33865943a498e76f6859060ea9b6d0b9b9e
Paolo Torres <paolo-torres@hotmail.com>
2019-11-22 12:10:03 -0500

    Create maximum_difference.cpp

---
+++ maximum_difference.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+int maximumDifference(vector<int>& nums) {
+    int minElement = nums[0];
+    int maxDiff = nums[1] - nums[0];
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] < minElement) {
+            minElement = nums[i];
+        }
+        if (nums[i] - minElement > maxDiff) {
+            maxDiff = nums[i] - minElement;
+        }
+    }
+    return maxDiff;
+}
+
+int main()
+{
+    vector<int> v1 = {2, 3, 10, 6, 4, 8, 1};
+    vector<int> v2 = {7, 9, 5, 6, 3, 2};
+    cout << "v1: " << maximumDifference(v1) << endl;
+    cout << "v2: " << maximumDifference(v2) << endl;
+
+    return 0;
+}
