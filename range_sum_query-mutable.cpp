b4fb58bc8cb4b383951db24054914c6b2420cdb6
Paolo Torres <paolo-torres@hotmail.com>
2019-11-30 14:19:29 -0500

    Create range_sum_query-mutable.cpp

---
+++ range_sum_query-mutable.cpp
@@ -0,0 +1,39 @@
+class NumArray {
+private:
+    vector<int> data;
+    vector<int> temp;
+public:
+    NumArray(vector<int>& nums) {
+        if (nums.empty()) {
+            return;
+        }
+        data = nums;
+        temp = nums;
+        for (int i = 1; i < nums.size(); i++) {
+            data[i] = data[i] + data[i-1];
+        }
+    }
+    
+    void update(int i, int val) {
+        int diff = temp[i] - val;
+        temp[i] = val;
+        for (int index = i; index < data.size(); index++) {
+            data[index] -= diff;
+        }
+    }
+    
+    int sumRange(int i, int j) {
+        if (i == 0) {
+            return data[j];
+        } else {
+            return data[j] - data[i-1];
+        }
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(i,val);
+ * int param_2 = obj->sumRange(i,j);
+ */
