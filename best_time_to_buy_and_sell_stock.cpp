5d6cbea7fb62db0b8430c39b6b43a7064e780937
Paolo Torres <paolo-torres@hotmail.com>
2019-12-01 18:43:13 -0500

    Create best_time_to_buy_and_sell_stock.cpp

---
+++ best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() < 2) {
+            return 0;
+        }
+        int minVal = prices[0];
+        int maxDiff = prices[1] - prices[0];
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < minVal) {
+                minVal = prices[i];
+            }
+            if (prices[i] - minVal > maxDiff) {
+                maxDiff = prices[i] - minVal;
+            }
+        }
+        return maxDiff;
+    }
+};
