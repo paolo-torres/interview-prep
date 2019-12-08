cab7a6ccde432ad9fda960d564151872f1ce92d1
Paolo Torres <paolo-torres@hotmail.com>
2019-12-07 22:00:54 -0500

    Create move_zeroes.cpp

---
+++ move_zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int start = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[start] = nums[i];
+                start++;
+            }
+        }
+        for (int i = start; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
