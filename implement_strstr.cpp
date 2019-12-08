85f7f6c066baddbe9e4c633e0d593ddfd8f0d54b
Paolo Torres <paolo-torres@hotmail.com>
2019-12-08 16:52:46 -0500

    Update implement_strstr.cpp

--- implement_strstr.cpp
+++ implement_strstr.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        return haystack.find(needle);
-    }   
+        if (needle.empty()) {
+            return 0;
+        }
+        int result = -1;
+        for (int i = 0; i < haystack.size(); i++) {
+            if (haystack[i] == needle[0]) {
+                if (haystack.substr(i, needle.size()) == needle) {
+                    result = i;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 };
