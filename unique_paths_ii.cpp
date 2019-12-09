bf63d4dbb822769281b1366bd0a3684056cb21e0
Paolo Torres <paolo-torres@hotmail.com>
2019-12-09 14:45:47 -0500

    Update unique_paths_ii.cpp

--- unique_paths_ii.cpp
+++ unique_paths_ii.cpp
@@ -6,31 +6,37 @@ public:
         }
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<unsigned int>> temp(m, vector<unsigned int>(n));
-        temp[0][0] = 1;
+        vector<vector<unsigned int>> data(m, vector<unsigned int>(n));
+        data[0][0] = 1;
         for (int i = 1; i < m; i++) {
-            if (obstacleGrid[i][0] == 0 && obstacleGrid[i-1][0] == 0) {
-                temp[i][0] = temp[i-1][0];
+            if (obstacleGrid[i][0] == 1) {
+                data[i][0] = 0;
+                break;
             } else {
-                temp[i][0] = 0;
+                data[i][0] = 1;
             }
         }
-        for (int i = 1; i < n; i++) {
-            if (obstacleGrid[0][i] == 0 && obstacleGrid[0][i-1] == 0) {
-                temp[0][i] = temp[0][i-1];
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[0][j] == 1) {
+                data[0][j] = 0;
+                break;
             } else {
-                temp[0][i] = 0;
+                data[0][j] = 1;
             }
         }
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                if (obstacleGrid[i][j] == 0) {
-                    temp[i][j] = temp[i-1][j] + temp[i][j-1];
+                if (obstacleGrid[i][j] == 1) {
+                    data[i][j] = 0;
                 } else {
-                    temp[i][j] = 0;
+                    data[i][j] = data[i-1][j] + data[i][j-1];
                 }
             }
         }
-        return temp[m-1][n-1];
+        if (obstacleGrid[m-1][n-1] == 1) {
+            return 0;
+        } else {
+            return data[m-1][n-1];
+        }
     }
 };
