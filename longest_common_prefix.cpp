949aba3822d98bb356563a8778fc88282e4dfc1e
Paolo Torres <paolo-torres@hotmail.com>
2019-12-08 20:58:11 -0500

    Update longest_common_prefix.cpp

--- longest_common_prefix.cpp
+++ longest_common_prefix.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string empty = "";
+        string empty;
         if (strs.empty()) {
             return empty;
         }
-        string prefix = strs[0];
+        string result = strs[0];
         for (int i = 1; i < strs.size(); i++) {
-            while (prefix != strs[i] && prefix != "") {
-                if (strs[i].size() > prefix.size()) {
-                    strs[i] = strs[i].substr(0, prefix.size());
+            while (result != strs[i]) {
+                if (result.size() < strs[i].size()) {
+                    strs[i] = strs[i].substr(0, result.size());
                 } else {
-                    prefix = prefix.substr(0, prefix.size() - 1);
+                    result = result.substr(0, result.size() - 1);
+                }
+                if (result.empty()) {
+                    return empty;
                 }
             }
-            if (prefix == "") {
-                return empty;
-            }
         }
-        return prefix;
+        return result;
     }
 };
