2311c2c6fcfb239f6f285c1bd627d5878ee88f30
Paolo Torres <paolo-torres@hotmail.com>
2019-12-13 15:44:31 -0500

    Update maximum_subarray.cpp

--- maximum_subarray.cpp
+++ maximum_subarray.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int result = nums[0];
-        if (nums.size() == 1) {
-            return result;
+        if (nums.empty()) {
+            return 0;
         }
         int temp = nums[0];
+        int result = temp;
         for (int i = 1; i < nums.size(); i++) {
             temp = max(nums[i], temp + nums[i]);
             result = max(result, temp);
