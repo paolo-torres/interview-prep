d353abfeea52424a040730d1c5b456076b868732
Paolo Torres <paolo-torres@hotmail.com>
2019-12-17 10:06:09 -0500

    Update contains_duplicate.cpp

--- contains_duplicate.cpp
+++ contains_duplicate.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        if (nums.size() < 2) {
-            return false;
-        }
-        sort(nums.begin(), nums.end());
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] == nums[i-1]) {
+        unordered_set<int> s;
+        for (int i = 0; i < nums.size(); i++) {
+            if (s.find(nums[i]) != s.end()) {
                 return true;
+            } else {
+                s.insert(nums[i]);
             }
         }
         return false;
