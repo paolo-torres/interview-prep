50b32951d08efc0cd6d8d4fb4b326c2d4ed29d2b
Paolo Torres <paolo-torres@hotmail.com>
2019-12-17 16:56:48 -0500

    Update fibonacci_numbers.cpp

--- fibonacci_numbers.cpp
+++ fibonacci_numbers.cpp
@@ -1,23 +1,20 @@
-#include <stdio.h>
-
-#include <iostream>
-
-using namespace std;
-
-int fib(int n) {
-    int arr[n+2];
-    arr[0] = 0;
-    arr[1] = 1;
-    for (int i = 2; i <= n; i++) {
-        arr[i] = arr[i-1] + arr[i-2];
+class Solution {
+public:
+    int fib(int N) {
+        if (N == 0) {
+            return 0;
+        }
+        if (N == 1) {
+            return 1;
+        }
+        int first = 0;
+        int second = 1;
+        int result = 0;
+        for (int i = 2; i <= N; i++) {
+            result = first + second;
+            first = second;
+            second = result;
+        }
+        return result;
     }
-    return arr[n];
-}
-
-int main()
-{
-    for (int i = 0; i < 47; i++) {
-        cout << "index: "<< i << " value: " << fib(i) << endl;
-    }
-    return 0;
-}
+};
