e9a43c77ee5ceed81fd0f06afc5c48d9179694cf
Paolo Torres <paolo-torres@hotmail.com>
2019-12-19 12:08:11 -0500

    Create decode_ways.cpp

---
+++ decode_ways.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        if (n == 0 || s[0] == '0') {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int first = 1;
+        int second = 1;
+        int result = first + second;
+        for (int i = 2; i <= n; i++) {
+            int firstTemp = first * (int)((s[i-2] == '1') || (s[i-2] == '2' && s[i-1] <= '6'));
+            int secondTemp = second * (int)(s[i-1] != '0');
+            result = firstTemp + secondTemp;
+            first = second;
+            second = result;
+        }
+        return result;
+    }
+};
