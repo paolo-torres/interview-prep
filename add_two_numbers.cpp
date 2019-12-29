c02dee59610284d3f285429ede1c1d0f9a51e8ca
Paolo Torres <paolo-torres@hotmail.com>
2019-12-29 18:01:42 -0500

    Update add_two_numbers.cpp

--- add_two_numbers.cpp
+++ add_two_numbers.cpp
@@ -10,59 +10,48 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
-        int temp = 0;
+        ListNode* prev = l1;
         ListNode* head = l1;
-        ListNode* trail = l1;
         while (l1 != NULL && l2 != NULL) {
             l1->val = l1->val + l2->val + carry;
-            carry = 0;
             if (l1->val >= 10) {
-                temp = l1->val;
+                carry = 1;
                 l1->val %= 10;
-                carry = temp / 10;
+            } else {
+                carry = 0;
             }
             if (l1->next == NULL) {
-                trail = l1;
+                prev = l1;
             }
             l1 = l1->next;
             l2 = l2->next;
         }
-        if (l1 != NULL) {
-            while (l1 != NULL) {
-                l1->val = l1->val + carry;
-                if (l1->val >= 10) {
-                    temp = l1->val;
-                    l1->val %= 10;
-                    carry = temp / 10;                
-                } else {
-                    carry = 0;
-                }
-                trail = l1;
-                l1 = l1->next;
+        while (l1 != NULL) {
+            l1->val = l1->val + carry;
+            if (l1->val >= 10) {
+                carry = 1;
+                l1->val %= 10;
+            } else {
+                carry = 0;
             }
+            prev = l1;
+            l1 = l1->next;
         }
-        if (l2 != NULL) {
-            while (l2 != NULL) {
-                trail->next = l2;
-                trail->next->val = l2->val + carry;
-                if (trail->next->val >= 10) {
-                    temp = trail->next->val;
-                    trail->next->val %= 10;
-                    carry = temp / 10;
-                } else {
-                    carry = 0;
-                }
-                trail = l2;
-                l2 = l2->next;
+        while (l2 != NULL) {
+            prev->next = l2;
+            prev->next->val = l2->val + carry;
+            if (prev->next->val >= 10) {
+                carry = 1;
+                prev->next->val %= 10;
+            } else {
+                carry = 0;
             }
+            prev = l2;
+            l2 = l2->next;
         }
-        if (carry != 0) {
-            ListNode* newNode = new ListNode(carry);
-            newNode->next = NULL;
-            while (trail->next != NULL) {
-                trail = trail->next;
-            }
-            trail->next = newNode;
+        if (carry == 1) {
+            ListNode* temp = new ListNode(carry);
+            prev->next = temp;
         }
         return head;
     }
