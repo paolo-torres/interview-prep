d2b0a39b695a4b5d2c4c7f1491edb1afe6969ac2
Paolo Torres <paolo-torres@hotmail.com>
2020-01-02 18:33:19 -0500

    Update construct_binary_tree_from_preorder_and_inorder.cpp

--- construct_binary_tree_from_preorder_and_inorder.cpp
+++ construct_binary_tree_from_preorder_and_inorder.cpp
@@ -8,25 +8,23 @@
  * };
  */
 class Solution {
-public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        if (preorder.size() == 0) {
+private:
+    TreeNode* build(vector<int>& preorder, int pStart, int pEnd, vector<int>& inorder, int iStart, int iEnd, unordered_map<int, int>& m) {
+        if (pStart > pEnd) {
             return NULL;
         }
-        TreeNode* root = new TreeNode(preorder[0]);
-        int i = 0;
-        for (int index = 0; index < inorder.size(); index++) {
-            if (inorder[index] == preorder[0]) {
-                i = index;
-                break;
-            }
-        }
-        vector<int> preorderLeft(preorder.begin() + 1, preorder.begin() + 1 + i);
-        vector<int> preorderRight(preorder.begin() + 1 + i, preorder.end());
-        vector<int> inorderLeft(inorder.begin(), inorder.begin() + i);
-        vector<int> inorderRight(inorder.begin() + i + 1, inorder.end());
-        root->left = buildTree(preorderLeft, inorderLeft);
-        root->right = buildTree(preorderRight, inorderRight);
+        TreeNode* root = new TreeNode(preorder[pStart]);
+        int i = m[preorder[pStart]];
+        root->left = build(preorder, pStart + 1, pStart + i - iStart, inorder, iStart, i - 1, m);
+        root->right = build(preorder, pStart + i - iStart + 1, pEnd, inorder, i + 1, iEnd, m);
         return root;
+    }
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < inorder.size(); i++) {
+            m[inorder[i]] = i;
+        }
+        return build(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1, m);
     }
 };
