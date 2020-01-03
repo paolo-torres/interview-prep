66fd3c80a9e2f09f69b5ef5f3d37307a12729538
Paolo Torres <paolo-torres@hotmail.com>
2020-01-02 23:42:52 -0500

    Update longest_substring_without_repeating_characters.cpp

--- longest_substring_without_repeating_characters.cpp
+++ longest_substring_without_repeating_characters.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if (s.empty()) {
-            return 0;
-        }
-        if (s.size() == 1) {
-            return 1;
-        }
-        unordered_set <char> mySet;
-        int curLength = 0;
-        int maxLength = 0;
-        auto it_temp = s.begin();
-        for (auto it_str = s.begin(); it_str != s.end(); ++it_str) {
-            auto it_set = mySet.find(*it_str);
-            if (curLength == 1) {
-                it_temp = it_str;
-            }
-            if (it_set != mySet.end()) {
-                curLength = 1;
-                mySet.clear();
-                it_str = it_temp;
+        unordered_set<char> letters;
+        int i = 0;
+        int j = 0;
+        int result = 0;
+        while (j < s.size()) {
+            if (letters.find(s[j]) != letters.end()) {
+                letters.erase(s[i]);
+                i++;
             } else {
-                curLength++;
-                if (curLength > maxLength) {
-                    maxLength = curLength;
-                }
+                letters.insert(s[j]);
+                j++;
+                result = max(result, j - i);
             }
-            mySet.insert(*it_str);
         }
-        return maxLength;
+        return result;
     }
 };
