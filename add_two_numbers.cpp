90e4fb1578f55128cd1fcee9cc50321dc58ffa16
Paolo Torres <paolo-torres@hotmail.com>
2020-01-05 18:03:58 -0500

    Update add_two_numbers.cpp

--- add_two_numbers.cpp
+++ add_two_numbers.cpp
@@ -10,18 +10,18 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
-        ListNode* prev = l1;
+        ListNode* temp = l1;
         ListNode* head = l1;
         while (l1 != NULL && l2 != NULL) {
             l1->val = l1->val + l2->val + carry;
             if (l1->val >= 10) {
-                carry = 1;
                 l1->val %= 10;
+                carry = 1;
             } else {
                 carry = 0;
             }
             if (l1->next == NULL) {
-                prev = l1;
+                temp = l1;
             }
             l1 = l1->next;
             l2 = l2->next;
@@ -29,29 +29,27 @@ public:
         while (l1 != NULL) {
             l1->val = l1->val + carry;
             if (l1->val >= 10) {
-                carry = 1;
                 l1->val %= 10;
+                carry = 1;
             } else {
                 carry = 0;
             }
-            prev = l1;
+            temp = l1;
             l1 = l1->next;
         }
         while (l2 != NULL) {
-            prev->next = l2;
-            prev->next->val = l2->val + carry;
-            if (prev->next->val >= 10) {
+            temp->next = new ListNode(l2->val + carry);
+            if (temp->next->val >= 10) {
+                temp->next->val %= 10;
                 carry = 1;
-                prev->next->val %= 10;
             } else {
                 carry = 0;
             }
-            prev = l2;
+            temp = temp->next;
             l2 = l2->next;
         }
         if (carry == 1) {
-            ListNode* temp = new ListNode(carry);
-            prev->next = temp;
+            temp->next = new ListNode(carry);
         }
         return head;
     }
