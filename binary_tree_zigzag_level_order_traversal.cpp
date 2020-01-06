4e254b3246fc567202b73eac1dc4a9472dd7791a
Paolo Torres <paolo-torres@hotmail.com>
2020-01-05 23:05:24 -0500

    Update binary_tree_zigzag_level_order_traversal.cpp

--- binary_tree_zigzag_level_order_traversal.cpp
+++ binary_tree_zigzag_level_order_traversal.cpp
@@ -8,35 +8,25 @@
  * };
  */
 class Solution {
+private:
+    void traverse(TreeNode* root, vector<vector<int>>& result, int level) {
+        if (root == NULL) {
+            return;
+        }
+        if (level == result.size()) {
+            result.push_back(vector<int>());
+        }
+        result[level].push_back(root->val);
+        traverse(root->left, result, level + 1);
+        traverse(root->right, result, level + 1);
+    }
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> result;
-        if (root == NULL) {
-            return result;
-        }
-        deque<TreeNode*> curr;
-        deque<TreeNode*> next;
-        curr.push_back(root);
-        vector<int> temp;
-        int level = 0;
-        while (!curr.empty()) {
-            auto it = curr.front();
-            curr.pop_front();
-            temp.push_back(it->val);
-            if (it->left != NULL) {
-                next.push_back(it->left);
-            }
-            if (it->right != NULL) {
-                next.push_back(it->right);
-            }
-            if (curr.empty()) {
-                if (level % 2 != 0) {
-                    reverse(temp.begin(), temp.end());
-                }
-                level++;
-                result.push_back(temp);
-                temp.clear();
-                swap(curr, next);
+        traverse(root, result, 0);
+        for (int i = 0; i < result.size(); i++) {
+            if (i % 2 != 0) {
+                reverse(result[i].begin(), result[i].end());
             }
         }
         return result;
