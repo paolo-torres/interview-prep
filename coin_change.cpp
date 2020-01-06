70d3c80af15fb5ed627ed3882f37be32ffa1702c
Paolo Torres <paolo-torres@hotmail.com>
2020-01-06 18:42:50 -0500

    Update coin_change.cpp

--- coin_change.cpp
+++ coin_change.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> result(amount + 1, amount + 1);
-        result[0] = 0;
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
             for (int j = 0; j < coins.size(); j++) {
                 if (coins[j] <= i) {
-                    result[i] = min(result[i], result[i-coins[j]] + 1);
+                    dp[i] = min(dp[i], dp[i-coins[j]] + 1);
                 }
             }
         }
-        if (result[amount] <= amount) {
-            return result[amount];
+        if (dp[amount] <= amount) {
+            return dp[amount];
         } else {
             return -1;
         }
