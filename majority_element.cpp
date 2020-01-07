1027eb86f14c70189c185b70c7bfa7192ed3d91a
Paolo Torres <paolo-torres@hotmail.com>
2020-01-07 08:40:05 -0500

    Update majority_element.cpp

--- majority_element.cpp
+++ majority_element.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        if (nums.empty()) {
-            return 0;
+        if (nums.size() == 1) {
+            return nums[0];
         }
         unordered_map<int, int> m;
-        for (int i = 0; i < nums.size(); i++) {
+        int n = nums.size();
+        int result = 0;
+        for (int i = 0; i < n; i++) {
             auto it = m.find(nums[i]);
             if (it != m.end()) {
                 it->second++;
+                if (it->second > n / 2) {
+                    result = it->first;
+                    break;
+                }
             } else {
                 m.insert({nums[i], 1});
             }
         }
-        priority_queue<pair<int, int>> pq;
-        for (auto it = m.begin(); it != m.end(); it++) {
-            pq.push({it->second, it->first});
-        }
-        return pq.top().second;
+        return result;
     }
 };
