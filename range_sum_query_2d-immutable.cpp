cc9775df65cecd74a3779d5f2773de96e7273f1f
Paolo Torres <paolo-torres@hotmail.com>
2020-01-07 17:18:24 -0500

    Update range_sum_query_2d-immutable.cpp

--- range_sum_query_2d-immutable.cpp
+++ range_sum_query_2d-immutable.cpp
@@ -7,15 +7,17 @@ public:
             return;
         }
         data = matrix;
-        for (int i = 1; i < matrix.size(); i++) {
-            data[i][0] = data[i][0] + data[i-1][0];
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for (int i = 1; i < m; i++) {
+            data[i][0] += data[i-1][0];
         }
-        for (int i = 1; i < matrix[0].size(); i++) {
-            data[0][i] = data[0][i] + data[0][i-1];
+        for (int j = 1; j < n; j++) {
+            data[0][j] += data[0][j-1];
         }
-        for (int i = 1; i < matrix.size(); i++) {
-            for (int j = 1; j < matrix[0].size(); j++) {
-                data[i][j] = data[i][j] + data[i-1][j] + data[i][j-1] - data[i-1][j-1];
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                data[i][j] += data[i-1][j] + data[i][j-1] - data[i-1][j-1];
             }
         }
     }
@@ -24,13 +26,13 @@ public:
         int a = 0;
         int b = 0;
         int c = 0;
-        if (row1 != 0) {
-            a = data[row1-1][col2];
+        if (col1 > 0) {
+            a = data[row2][col1-1];
         }
-        if (col1 != 0) {
-            b = data[row2][col1-1];
+        if (row1 > 0) {
+            b = data[row1-1][col2];
         }
-        if (row1 != 0 && col1 != 0) {
+        if (col1 > 0 && row1 > 0) {
             c = data[row1-1][col1-1];
         }
         return data[row2][col2] - a - b + c;
