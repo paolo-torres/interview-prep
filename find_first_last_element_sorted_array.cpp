22cecbae30bfa75e1d014dd429d4184a5aa51de2
Paolo Torres <paolo-torres@hotmail.com>
2020-01-06 19:44:10 -0500

    Update find_first_last_element_sorted_array.cpp

--- find_first_last_element_sorted_array.cpp
+++ find_first_last_element_sorted_array.cpp
@@ -1,44 +1,36 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> result;
+        vector<int> result = {-1, -1};
         if (nums.empty()) {
-            result.push_back(-1);
-            result.push_back(-1);
             return result;
         }
         int low = 0;
         int high = nums.size() - 1;
-        while (low <= high) {
+        while (low < high) {
             int mid = low + (high - low) / 2;
-            if (target < nums[mid]) {
-                high = mid - 1;
-            } else if (target > nums[mid]) {
+            if (target > nums[mid]) {
                 low = mid + 1;
             } else {
-                low = mid;
-                while (nums[low] == nums[mid]
-                    && low != 0) {
-                    low--;
-                }
-                if (nums[low] != nums[mid]) {
-                    low++;
-                }
                 high = mid;
-                while (nums[high] == nums[mid]
-                    && high != nums.size() - 1) {
-                    high++;
-                }
-                if (nums[high] != nums[mid]) {
-                    high--;
-                }
-                result.push_back(low);
-                result.push_back(high);
-                return result;
             }
         }
-        result.push_back(-1);
-        result.push_back(-1);
+        if (target == nums[low]) {
+            result[0] = low;
+        }
+        low = 0;
+        high = nums.size() - 1;
+        while (low < high) {
+            int mid = low + 1 + (high - low) / 2;
+            if (target >= nums[mid]) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        if (target == nums[high]) {
+            result[1] = high;
+        }
         return result;
     }
 };
