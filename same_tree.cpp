d52180acb302df72adc10eff9ada7bd26174b448
Paolo Torres <paolo-torres@hotmail.com>
2020-01-07 22:41:45 -0500

    Update same_tree.cpp

--- same_tree.cpp
+++ same_tree.cpp
@@ -8,24 +8,17 @@
  * };
  */
 class Solution {
-private:
-    bool isSame(TreeNode* p, TreeNode* q) {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
         if (p == NULL && q == NULL) {
             return true;
         }
-        if (p != NULL && q == NULL || p == NULL && q != NULL) {
+        if (p == NULL || q == NULL) {
             return false;
         }
         if (p->val != q->val) {
             return false;
         }
-        return isSame(p->left, q->left) && isSame(p->right, q->right);
-    }
-public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (isSame(p, q)) {
-            return true;
-        }
-        return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
