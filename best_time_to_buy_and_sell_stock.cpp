470db06059ef42c590decb31264518a177e300d8
Paolo Torres <paolo-torres@hotmail.com>
2020-10-19 17:35:32 -0400

    Update best_time_to_buy_and_sell_stock.cpp

--- best_time_to_buy_and_sell_stock.cpp
+++ best_time_to_buy_and_sell_stock.cpp
@@ -4,15 +4,11 @@ public:
         if (prices.size() < 2) {
             return 0;
         }
-        int minVal = prices[0];
+        int minValue = prices[0];
         int maxDiff = 0;
         for (int i = 1; i < prices.size(); i++) {
-            if (prices[i] < minVal) {
-                minVal = prices[i];
-            }
-            if (prices[i] - minVal > maxDiff) {
-                maxDiff = prices[i] - minVal;
-            }
+            minValue = min(minValue, prices[i]);
+            maxDiff = max(maxDiff, prices[i] - minValue);
         }
         return maxDiff;
     }
