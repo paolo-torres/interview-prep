1be1e4c5baffd092a189d2c572005b1c0b8ca0d6
Paolo Torres <paolo-torres@hotmail.com>
2021-05-16 15:06:26 -0400

    12/75

--- reverse_linked_list.cpp
+++ reverse_linked_list.cpp
@@ -1,9 +1,14 @@
+// Time: O(n)
+// Space: O(1)
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
@@ -13,15 +18,13 @@ public:
             return head;
         }
         ListNode* prev = NULL;
-        ListNode* curr = head;
         ListNode* next = head->next;
-        while (curr != NULL) {
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
+        while (head != NULL) {
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
         }
-        head = prev;
-        return head;
+        return prev;
     }
 };
