fd5beae32de329c0e3064b4f4b21bfcb060241b4
Paolo Torres <paolo-torres@hotmail.com>
2021-05-20 17:44:08 -0400

    16/75

--- merge_k_sorted_lists.cpp
+++ merge_k_sorted_lists.cpp
@@ -1,32 +1,43 @@
+// Time: O(n log k)
+// Space: O(n)
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         priority_queue<int, vector<int>, greater<int>> pq;
+        
         for (int i = 0; i < lists.size(); i++) {
-            while (lists[i] != NULL) {
-                pq.push(lists[i]->val);
-                lists[i] = lists[i]->next;
+            ListNode* node = lists[i];
+            while (node != NULL) {
+                pq.push(node->val);
+                node = node->next;
             }
         }
+        
         if (pq.empty()) {
             return NULL;
         }
-        ListNode* root = new ListNode(pq.top());
-        ListNode* head = root;
+        
+        ListNode* curr = new ListNode(pq.top());
+        ListNode* head = curr;
         pq.pop();
+        
         while (!pq.empty()) {
-            root->next = new ListNode(pq.top());
-            root = root->next;
+            curr->next = new ListNode(pq.top());
+            curr = curr->next;
             pq.pop();
         }
+        
         return head;
     }
 };
