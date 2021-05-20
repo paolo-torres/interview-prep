79f1c2695bac3f008b561ee7475e83580d231225
Paolo Torres <paolo-torres@hotmail.com>
2021-05-20 19:46:45 -0400

    17/75

--- best_time_to_buy_and_sell_stock.cpp
+++ best_time_to_buy_and_sell_stock.cpp
@@ -1,15 +1,17 @@
+// Time: O(n)
+// Space: O(1)
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if (prices.size() < 2) {
-            return 0;
-        }
         int minValue = prices[0];
         int maxDiff = 0;
+        
         for (int i = 1; i < prices.size(); i++) {
             minValue = min(minValue, prices[i]);
             maxDiff = max(maxDiff, prices[i] - minValue);
         }
+        
         return maxDiff;
     }
 };
