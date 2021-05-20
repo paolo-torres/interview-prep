320ac6812c531322599d296e456ddcd98f0b37dc
Paolo Torres <paolo-torres@hotmail.com>
2021-05-20 15:32:58 -0400

    14/75

--- longest_substring_without_repeating_characters.cpp
+++ longest_substring_without_repeating_characters.cpp
@@ -1,20 +1,26 @@
+// Time: O(n)
+// Space: O(n)
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         unordered_set<char> letters;
+
         int i = 0;
         int j = 0;
         int result = 0;
+        
         while (j < s.size()) {
-            if (letters.find(s[j]) != letters.end()) {
-                letters.erase(s[i]);
-                i++;
-            } else {
+            if (letters.find(s[j]) == letters.end()) {
                 letters.insert(s[j]);
                 j++;
                 result = max(result, j - i);
+            } else {
+                letters.erase(s[i]);
+                i++;
             }
         }
+
         return result;
     }
 };
