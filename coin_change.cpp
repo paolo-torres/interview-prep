aaff2bbe1aafe10014d80331254c561706d940c9
Paolo Torres <paolo-torres@hotmail.com>
2021-05-22 15:25:42 -0400

    19/75

--- coin_change.cpp
+++ coin_change.cpp
@@ -1,9 +1,12 @@
+// Time: O(n x amount)
+// Space: O(amount)
+
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount + 1, amount + 1);
         dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
+        for (int i = 1; i < dp.size(); i++) {
             for (int j = 0; j < coins.size(); j++) {
                 if (coins[j] <= i) {
                     dp[i] = min(dp[i], dp[i-coins[j]] + 1);
@@ -12,8 +15,7 @@ public:
         }
         if (dp[amount] <= amount) {
             return dp[amount];
-        } else {
-            return -1;
         }
+        return -1;
     }
 };
