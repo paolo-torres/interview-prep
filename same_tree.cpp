1cffea3b6807e2bb6abf259cd4df081b3ad4a3c5
Paolo Torres <paolo-torres@hotmail.com>
2021-06-07 16:03:17 -0400

    25/75

--- same_tree.cpp
+++ same_tree.cpp
@@ -1,10 +1,15 @@
+// Time: O(n)
+// Space: O(n)
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
