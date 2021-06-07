fbe28ac4bb415c8a763b07cf4b1bb6e22d2bf20a
Paolo Torres <paolo-torres@hotmail.com>
2021-06-07 17:15:01 -0400

    26/75

--- top_k_frequent_elements.cpp
+++ top_k_frequent_elements.cpp
@@ -1,24 +1,25 @@
+// Time: O(n log n)
+// Space: O(n + k)
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> m;
         for (int i = 0; i < nums.size(); i++) {
-            auto it = m.find(nums[i]);
-            if (it != m.end()) {
-                it->second++;
-            } else {
-                m.insert({nums[i], 1});
-            }
+            m[nums[i]]++;
         }
+        
         priority_queue<pair<int, int>> pq;
         for (auto it = m.begin(); it != m.end(); it++) {
             pq.push({it->second, it->first});
         }
+        
         vector<int> result;
         for (int i = 0; i < k; i++) {
             result.push_back(pq.top().second);
             pq.pop();
         }
+        
         return result;
     }
 };
