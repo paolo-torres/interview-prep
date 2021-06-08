a41e760a709675cb2b6ff13537b0818ae9af42c6
Paolo Torres <paolo-torres@hotmail.com>
2021-06-07 20:29:28 -0400

    update 28/75

--- counting_bits.cpp
+++ counting_bits.cpp
@@ -6,7 +6,7 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans(n + 1, 0);
         for (int i = 1; i <= n; i++) {
-            ans[i] = ans[i & (i - 1)] + 1;
+            ans[i] = ans[i >> 1] + (i & 1);
         }
         return ans;
     }
