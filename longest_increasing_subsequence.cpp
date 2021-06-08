7907f695bd5ffdf008cbb5ca2fceff35fa7e1586
Paolo Torres <paolo-torres@hotmail.com>
2021-06-07 21:47:57 -0400

    29/75

--- longest_increasing_subsequence.cpp
+++ longest_increasing_subsequence.cpp
@@ -1,20 +1,24 @@
+// Time: O(n^2)
+// Space: O(n)
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         if (nums.empty()) {
             return 0;
         }
-        vector<int> dp(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<int> dp(n, 0);
         dp[0] = 1;
         int result = 1;
-        for (int i = 1; i < dp.size(); i++) {
-            int temp = 0;
+        for (int i = 1; i < n; i++) {
+            int maxValue = 0;
             for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    temp = max(temp, dp[j]);
+                if (nums[j] < nums[i]) {
+                    maxValue = max(maxValue, dp[j]);
                 }
             }
-            dp[i] = temp + 1;
+            dp[i] = maxValue + 1;
             result = max(result, dp[i]);
         }
         return result;
