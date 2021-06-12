bef8e1f4de1119033923419d1b97d2253fa1307c
Paolo Torres <paolo-torres@hotmail.com>
2021-06-12 14:32:56 -0400

    33/75

--- rotate_image.cpp
+++ rotate_image.cpp
@@ -1,3 +1,6 @@
+// Time: O(n x n)
+// Space: O(1)
+
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
@@ -6,8 +9,6 @@ public:
             for (int j = i; j < n; j++) {
                 swap(matrix[i][j], matrix[j][i]);
             }
-        }
-        for (int i = 0; i < n; i++) {
             reverse(matrix[i].begin(), matrix[i].end());
         }
     }
