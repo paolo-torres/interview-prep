c90bba5dc582e3539f971fb572b054cb74541c20
Paolo Torres <paolo-torres@hotmail.com>
2021-06-11 20:16:48 -0400

    31/75

--- non-overlapping_intervals.cpp
+++ non-overlapping_intervals.cpp
@@ -1,24 +1,27 @@
+// Time: O(n log n)
+// Space: O(1)
+
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         if (intervals.size() < 2) {
             return 0;
         }
+        
         sort(intervals.begin(), intervals.end(), [](const auto& a, const auto& b) {
             return a[0] < b[0];
         });
-        int last = 0;
+        
         int result = 0;
-        for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[last][1] <= intervals[i][0]) {
-                last = i;
-            } else {
+        
+        for (int i = 0; i < intervals.size() - 1; i++) {
+            if (intervals[i][1] > intervals[i+1][0]) {
+                intervals[i+1][0] = intervals[i][0];
+                intervals[i+1][1] = min(intervals[i][1], intervals[i+1][1]);
                 result++;
-                if (intervals[last][1] > intervals[i][1]) {
-                    last = i;
-                }
             }
         }
+        
         return result;
     }
 };
