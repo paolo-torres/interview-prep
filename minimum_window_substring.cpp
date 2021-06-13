2efb65ffb1cda635bd5a4d9033a790e178685fd3
Paolo Torres <paolo-torres@hotmail.com>
2021-06-13 16:19:01 -0400

    34/75

--- minimum_window_substring.cpp
+++ minimum_window_substring.cpp
@@ -1,36 +1,46 @@
+// Time: O(m + n)
+// Space: O(m + n)
+
 class Solution {
 public:
     string minWindow(string s, string t) {
-        string result;
-        if (s.empty() || t.empty()) {
-            return result;
-        }
         unordered_map<char, int> m;
-        unordered_map<char, int> window;
+        
         for (int i = 0; i < t.size(); i++) {
             m[t[i]]++;
         }
-        int count = 0;
+        
+        int i = 0;
+        int j = 0;
+        
+        int counter = t.size();
+        int minStart = 0;
         int minLength = INT_MAX;
-        for (int left = 0, right = 0; right < s.size(); right++) {
-            char c = s[right];
-            if (m.find(c) != m.end()) {
-                window[c]++;
-                if (window[c] <= m[c]) {
-                    count++;
-                }
+        
+        while (j < s.size()) {
+            if (m[s[j]] > 0) {
+                counter--;
             }
-            if (count >= t.size()) {
-                while (m.find(s[left]) == m.end() || window[s[left]] > m[s[left]]) {
-                    window[s[left]]--;
-                    left++;
+            m[s[j]]--;
+            j++;
+            
+            while (counter == 0) {
+                if (j - i < minLength) {
+                    minStart = i;
+                    minLength = j - i;
                 }
-                if (right - left + 1 < minLength) {
-                    minLength = right - left + 1;
-                    result = s.substr(left, minLength);
+                
+                m[s[i]]++;
+                if (m[s[i]] > 0) {
+                    counter++;
                 }
+                i++;
             }
         }
-        return result;
+        
+        if (minLength != INT_MAX) {
+            return s.substr(minStart, minLength);
+        }
+        return "";
     }
 };
