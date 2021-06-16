aeb1ce85d12644c9e4c419a6b5c90cd7e2313429
Paolo Torres <paolo-torres@hotmail.com>
2021-06-16 19:20:01 -0400

    40/75

--- number_of_islands.cpp
+++ number_of_islands.cpp
@@ -1,28 +1,34 @@
+// Time: O(m x n)
+// Space: O(m x n)
+
 class Solution {
-private:
-    void findLand(vector<vector<char>>& grid, int i, int j, int m, int n) {
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') {
-            return;
-        }
-        grid[i][j] = '0';
-        findLand(grid, i - 1, j, m, n);
-        findLand(grid, i + 1, j, m, n);
-        findLand(grid, i, j - 1, m, n);
-        findLand(grid, i, j + 1, m, n);
-    }
 public:
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+        
         int result = 0;
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
-                    ++result;
-                    findLand(grid, i, j, m, n);
+                    explore(grid, i, j, m, n);
+                    result++;
                 }
             }
         }
+        
         return result;
+    }
+private:
+    void explore(vector<vector<char>>& grid, int i, int j, int m, int n) {
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        explore(grid, i - 1, j, m, n);
+        explore(grid, i + 1, j, m, n);
+        explore(grid, i, j - 1, m, n);
+        explore(grid, i, j + 1, m, n);
     }
 };
