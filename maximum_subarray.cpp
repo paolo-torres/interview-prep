9af6e383b068d12aa5e626fa1f6b903be164715b
Paolo Torres <paolo-torres@hotmail.com>
2021-06-21 21:19:20 -0400

    45/75

--- maximum_subarray.cpp
+++ maximum_subarray.cpp
@@ -1,12 +1,17 @@
+// Time: O(n)
+// Space: O(1)
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int temp = nums[0];
-        int result = temp;
+        int curr = nums[0];
+        int result = nums[0];
+        
         for (int i = 1; i < nums.size(); i++) {
-            temp = max(nums[i], temp + nums[i]);
-            result = max(result, temp);
+            curr = max(curr + nums[i], nums[i]);
+            result = max(result, curr);
         }
+        
         return result;
     }
 };
