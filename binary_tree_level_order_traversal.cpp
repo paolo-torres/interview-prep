f6e539ae42b56c80542cba8c74f33c7f6aa07daa
Paolo Torres <paolo-torres@hotmail.com>
2021-06-28 15:02:48 -0400

    51/75

--- binary_tree_level_order_traversal.cpp
+++ binary_tree_level_order_traversal.cpp
@@ -1,29 +1,50 @@
+// Time: O(n)
+// Space: O(n)
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-private:
-    void traverse(TreeNode* root, vector<vector<int>>& result, int level) {
-        if (root == NULL) {
-            return;
-        }
-        if (level == result.size()) {
-            result.push_back(vector<int>());
-        }
-        result[level].push_back(root->val);
-        traverse(root->left, result, level + 1);
-        traverse(root->right, result, level + 1);
-    }
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> result;
-        traverse(root, result, 0);
+        
+        if (root == NULL) {
+            return result;
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            int count = q.size();
+            vector<int> currLevel;
+            
+            for (int i = 0; i < count; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                currLevel.push_back(node->val);
+                
+                if (node->left != NULL) {
+                    q.push(node->left);
+                }
+                
+                if (node->right != NULL) {
+                    q.push(node->right);
+                }
+            }
+            
+            result.push_back(currLevel);
+        }
+        
         return result;
     }
 };
