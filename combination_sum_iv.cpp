914dcabfda4c167966dc145e32f78642e790aedc
Paolo Torres <paolo-torres@hotmail.com>
2021-06-29 15:08:06 -0400

    53/75

---
+++ combination_sum_iv.cpp
@@ -0,0 +1,20 @@
+// Time: O(n * target)
+// Space: O(target)
+
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> dp(target + 1);
+        dp[0] = 1;
+        
+        for (int i = 1; i < dp.size(); i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                if (i - nums[j] >= 0) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        
+        return dp[target];
+    }
+};
