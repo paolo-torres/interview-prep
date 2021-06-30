ef434214d8a98c0f9f5be016fe2255f9b2221752
Paolo Torres <paolo-torres@hotmail.com>
2021-06-29 20:47:33 -0400

    55/75

--- valid_parentheses.cpp
+++ valid_parentheses.cpp
@@ -1,27 +1,34 @@
+// Time: O(n)
+// Space: O(n)
+
 class Solution {
 public:
     bool isValid(string s) {
-        if (s.empty()) {
-            return true;
-        }
         stack<char> open;
+        
         for (int i = 0; i < s.size(); i++) {
-            if ((s[i] == ')' || s[i] == '}' || s[i] == ']') && open.empty()) {
-                return false;
-            } else if (s[i] == ')' && open.top() != '('
-                || s[i] == '}' && open.top() != '{'
-                || s[i] == ']' && open.top() != '[') {
-                return false;
-            } else if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
-                open.push(s[i]);
-            } else {
+            if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
+                if (open.empty()) {
+                    return false;
+                }
+                if (s[i] == ')' && open.top() != '(') {
+                    return false;
+                }
+                if (s[i] == '}' && open.top() != '{') {
+                    return false;
+                }
+                if (s[i] == ']' && open.top() != '[') {
+                    return false;
+                }
                 open.pop();
+            } else {
+                open.push(s[i]);
             }
         }
-        if (open.empty()) {
-            return true;
-        } else {
+        
+        if (!open.empty()) {
             return false;
         }
+        return true;
     }
 };
