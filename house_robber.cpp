a88d629204da4affd97131ed04185d5bf51ff2de
Paolo Torres <paolo-torres@hotmail.com>
2021-07-01 16:42:09 -0400

    58/75

--- house_robber.cpp
+++ house_robber.cpp
@@ -1,18 +1,23 @@
+// Time: O(n)
+// Space: O(n)
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.empty()) {
-            return 0;
-        } else if (nums.size() == 1) {
+        int n = nums.size();
+        
+        if (n == 1) {
             return nums[0];
         }
-        int n = nums.size();
-        vector<int> result(n);
-        result[0] = nums[0];
-        result[1] = max(nums[0], nums[1]);
+        
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        
         for (int i = 2; i < n; i++) {
-            result[i] = max(nums[i] + result[i-2], result[i-1]);
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);
         }
-        return result[n-1];
+        
+        return dp[n-1];
     }
 };
