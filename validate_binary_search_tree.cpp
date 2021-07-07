c6b3c77ebba7bddd9f4b8b5bdeaf7942830478a3
Paolo Torres <paolo-torres@hotmail.com>
2021-07-07 17:30:23 -0400

    68/75

--- validate_binary_search_tree.cpp
+++ validate_binary_search_tree.cpp
@@ -1,33 +1,42 @@
+// Time: O(n)
+// Space: O(1)
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        TreeNode* prev = NULL;
+        return isValid(root, prev);
+    }
 private:
-    bool checkBST(TreeNode* root, TreeNode* &prev) {
+    bool isValid(TreeNode* root, TreeNode* &prev) {
         if (root == NULL) {
             return true;
         }
-        if (!checkBST(root->left, prev)) {
+        
+        if (!isValid(root->left, prev)) {
             return false;
         }
+        
         if (prev != NULL && prev->val >= root->val) {
             return false;
         }
         prev = root;
-        if (!checkBST(root->right, prev)) {
+        
+        if (!isValid(root->right, prev)) {
             return false;
         }
+        
         return true;
-    }
-public:
-    bool isValidBST(TreeNode* root) {
-        TreeNode* prev = NULL;
-        return checkBST(root, prev);
     }
 };
