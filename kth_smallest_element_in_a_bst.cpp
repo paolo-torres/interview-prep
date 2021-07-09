568894528a7fa3ec38e44335142e1a647160ffc1
Paolo Torres <paolo-torres@hotmail.com>
2021-07-08 20:21:58 -0400

    71/75

--- kth_smallest_element_in_a_bst.cpp
+++ kth_smallest_element_in_a_bst.cpp
@@ -1,26 +1,38 @@
+// Time: O(n)
+// Space: O(h)
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-private:
-    void inorder(TreeNode* root, vector<int>& v) {
-        if (root == NULL) {
-            return;
-        }
-        inorder(root->left, v);
-        v.push_back(root->val);
-        inorder(root->right, v);
-    }
 public:
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> v;
-        inorder(root, v);
-        return v[k-1];
+        int result = 0;
+        kTraverse(root, k, result);
+        return result;
+    }
+private:
+    void kTraverse(TreeNode* root, int& k, int& result) {
+        if (root == NULL || k == 0) {
+            return;
+        }
+        
+        kTraverse(root->left, k, result);
+        
+        k--;
+        if (k == 0) {
+            result = root->val;
+            return;
+        }
+        
+        kTraverse(root->right, k, result);
     }
 };
