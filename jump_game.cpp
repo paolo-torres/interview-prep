116dc7f054033d6e5ffd2e097efb80f6fb39610f
Paolo Torres <paolo-torres@hotmail.com>
2021-07-09 21:31:34 -0400

    72/75

--- jump_game.cpp
+++ jump_game.cpp
@@ -1,30 +1,22 @@
+// Time: O(n)
+// Space: O(1)
+
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        if (nums.empty()) {
-            return false;
-        }
-        if (nums.size() == 1) {
-            return true;
-        }
-        int i = 0;
-        while (i < nums.size() - 1) {
-            if (i + nums[i] >= nums.size() - 1) {
-                return true;
-            }
-            if (nums[i] == 0) {
+        int n = nums.size();
+        int reachable = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (i > reachable) {
                 return false;
             }
-            int maxIndex = i + 1;
-            int maxValue = 0;
-            for (int j = i + 1; j < i + 1 + nums[i]; j++) {
-                if (j + nums[j] > maxValue) {
-                    maxIndex = j;
-                    maxValue = j + nums[j];
-                }
+            reachable = max(reachable, i + nums[i]);
+            if (reachable >= n) {
+                return true;
             }
-            i = maxIndex;
         }
-        return false;
+        
+        return true;
     }
 };
