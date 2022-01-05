5fcea0fd901667b5857a4b102b3dbc93232dfe61
Paolo Torres <paolo-torres@hotmail.com>
2022-01-04 22:41:00 -0800

    reorganize, start sp patterns 3/171

--- problems/contains_duplicate.cpp
+++ problems/contains_duplicate.cpp
@@ -5,12 +5,14 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> s;
+        
         for (int i = 0; i < nums.size(); i++) {
             if (s.find(nums[i]) != s.end()) {
                 return true;
             }
             s.insert(nums[i]);
         }
+        
         return false;
     }
 };
