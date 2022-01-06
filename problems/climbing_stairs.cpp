dc6cc1de5f215c40948ebaaca272f5a659376ee2
Paolo Torres <paolo-torres@hotmail.com>
2022-01-05 22:06:33 -0800

    sp patterns 6/171

--- problems/climbing_stairs.cpp
+++ problems/climbing_stairs.cpp
@@ -10,14 +10,18 @@ public:
         if (n == 2) {
             return 2;
         }
+        
         int first = 1;
         int second = 2;
+        
         int result = 0;
+        
         for (int i = 2; i < n; i++) {
             result = first + second;
             first = second;
             second = result;
         }
+        
         return result;
     }
 };
--- problems/single_number.cpp
+++ problems/single_number.cpp
@@ -1,22 +1,15 @@
+// Time: O(n)
+// Space: O(1)
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> m;
-        for (int i = 0; i < nums.size(); i++) {
-            auto it = m.find(nums[i]);
-            if (it != m.end()) {
-                it->second++;
-            } else {
-                m.insert({nums[i], 1});
-            }
-        }
         int result = 0;
-        for (auto it = m.begin(); it != m.end(); ++it) {
-            if (it->second == 1) {
-                result = it->first;
-                break;
-            }
+        
+        for (int i = 0; i < nums.size(); i++) {
+            result = result ^ nums[i];
         }
+        
         return result;
     }
 };
