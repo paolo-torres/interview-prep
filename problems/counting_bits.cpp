d9f213037491baafbc0b1f3453b1e937b0b6d6a9
Paolo Torres <paolo-torres@hotmail.com>
2022-01-06 23:15:30 -0800

    sp patterns 9/171

--- problems/counting_bits.cpp
+++ problems/counting_bits.cpp
@@ -4,10 +4,12 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n + 1, 0);
+        vector<int> result(n + 1, 0);
+        
         for (int i = 1; i <= n; i++) {
-            ans[i] = ans[i >> 1] + (i & 1);
+            result[i] = result[i >> 1] + (i & 1);
         }
-        return ans;
+        
+        return result;
     }
 };
--- problems/range_sum_query-immutable.cpp
+++ problems/range_sum_query-immutable.cpp
@@ -1,28 +1,32 @@
+// NumArray
+// Time: O(n)
+// Space: O(n)
+
+// sumRange
+// Time: O(1)
+// Space: O(1)
+
 class NumArray {
-private:
-    vector<int> data;
 public:
     NumArray(vector<int>& nums) {
-        if (nums.empty()) {
-            return;
-        }
         data = nums;
-        for (int i = 1; i < nums.size(); i++) {
-            data[i] = data[i] + data[i-1];
+        for (int i = 1; i < data.size(); i++) {
+            data[i] += data[i-1];
         }
     }
     
-    int sumRange(int i, int j) {
-        if (i == 0) {
-            return data[j];
-        } else {
-            return data[j] - data[i-1];
+    int sumRange(int left, int right) {
+        if (left == 0) {
+            return data[right];
         }
+        return data[right] - data[left-1];
     }
+private:
+    vector<int> data;
 };
 
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray* obj = new NumArray(nums);
- * int param_1 = obj->sumRange(i,j);
+ * int param_1 = obj->sumRange(left,right);
  */
