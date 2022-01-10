e45cd1835c7a96d5d7201a7f76903244ec58f595
Paolo Torres <paolo-torres@hotmail.com>
2022-01-09 16:21:51 -0800

    sp patterns 22/171

--- problems/minimum_depth_of_binary_tree.cpp
+++ problems/minimum_depth_of_binary_tree.cpp
@@ -1,10 +1,15 @@
+// Time: O(n)
+// Space: O(h)
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
@@ -13,12 +18,22 @@ public:
         if (root == NULL) {
             return 0;
         }
-        if (root->left != NULL && root->right != NULL) {
-            return 1 + min(minDepth(root->left), minDepth(root->right));
-        } else if (root->left != NULL) {
-            return 1 + minDepth(root->left);
-        } else {
-            return 1 + minDepth(root->right);
+        
+        int curr = 1;
+        int result = INT_MAX;
+        
+        findDepth(root, curr, result);
+        return result;
+    }
+private:
+    void findDepth(TreeNode* root, int curr, int& result) {
+        if (root == NULL) {
+            return;
         }
+        if (root->left == NULL && root->right == NULL) {
+            result = min(result, curr);
+        }
+        findDepth(root->left, curr + 1, result);
+        findDepth(root->right, curr + 1, result);
     }
 };
