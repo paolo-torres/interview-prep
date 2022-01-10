bae84ab949780d831d6610987663d161ca86cc41
Paolo Torres <paolo-torres@hotmail.com>
2022-01-09 22:39:10 -0800

    sp patterns 25/171

--- problems/diameter_of_binary_tree.cpp
+++ problems/diameter_of_binary_tree.cpp
@@ -1,27 +1,34 @@
+// Time: O(n)
+// Space: O(n)
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-private:
-    int traverse(TreeNode* root, int& result) {
-        if (root == NULL) {
-            return 0;
-        }
-        int left = traverse(root->left, result);
-        int right = traverse(root->right, result);
-        result = max(result, left + right);
-        return max(left, right) + 1;
-    }
 public:
     int diameterOfBinaryTree(TreeNode* root) {
         int result = 0;
         traverse(root, result);
         return result;
+    }
+private:
+    int traverse(TreeNode* root, int& result) {
+        if (root == NULL) {
+            return 0;
+        }
+        
+        int left = traverse(root->left, result);
+        int right = traverse(root->right, result);
+        
+        result = max(result, left + right);
+        return 1 + max(left, right);
     }
 };
--- problems/path_sum.cpp
+++ problems/path_sum.cpp
@@ -1,28 +1,37 @@
+// Time: O(n)
+// Space: O(n)
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool hasPathSum(TreeNode* root, int sum) {
+    bool hasPathSum(TreeNode* root, int targetSum) {
         if (root == NULL) {
             return false;
         }
-        sum -= root->val;
-        if (sum == 0 && root->left == NULL && root->right == NULL) {
-            return true;
+        int currSum = 0;
+        return hasSum(root, currSum, targetSum);
+    }
+private:
+    bool hasSum(TreeNode* root, int currSum, int targetSum) {
+        if (root == NULL) {
+            return false;
         }
-        if (hasPathSum(root->left, sum)) {
-            return true;
+        currSum += root->val;
+        if (root->left == NULL && root->right == NULL) {
+            if (currSum == targetSum) {
+                return true;
+            }
         }
-        if (hasPathSum(root->right, sum)) {
-            return true;
-        }
-        return false;
+        return hasSum(root->left, currSum, targetSum) || hasSum(root->right, currSum, targetSum);
     }
 };
