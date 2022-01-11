458913792004202b5892a7b1c5db35e013e3bd55
Paolo Torres <paolo-torres@hotmail.com>
2022-01-10 22:01:37 -0800

    sp patterns 28/171

--- problems/merge_two_binary_trees.cpp
+++ problems/merge_two_binary_trees.cpp
@@ -1,24 +1,32 @@
+// Time: O(n)
+// Space: O(n)
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
-        if (t1 == NULL) {
-            return t2;
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if (root1 == NULL) {
+            return root2;
         }
-        if (t2 == NULL) {
-            return t1;
+        if (root2 == NULL) {
+            return root1;
         }
-        t1->val += t2->val;
-        t1->left = mergeTrees(t1->left, t2->left);
-        t1->right = mergeTrees(t1->right, t2->right);
-        return t1;
+        
+        root1->val += root2->val;
+        
+        root1->left = mergeTrees(root1->left, root2->left);
+        root1->right = mergeTrees(root1->right, root2->right);
+        
+        return root1;
     }
 };
