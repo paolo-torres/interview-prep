624484b036d02eef304694bb4f0adf1b63828427
Paolo Torres <paolo-torres@hotmail.com>
2022-01-14 00:23:45 -0800

    sp patterns 35/171

--- problems/majority_element.cpp
+++ problems/majority_element.cpp
@@ -1,24 +1,24 @@
+// Time: O(n)
+// Space: O(1)
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        if (nums.size() == 1) {
-            return nums[0];
-        }
-        unordered_map<int, int> m;
-        int n = nums.size();
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            auto it = m.find(nums[i]);
-            if (it != m.end()) {
-                it->second++;
-                if (it->second > n / 2) {
-                    result = it->first;
-                    break;
-                }
+        int count = 1;
+        int result = nums[0];
+        
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == result) {
+                count++;
             } else {
-                m.insert({nums[i], 1});
+                count--;
+                if (count == 0) {
+                    result = nums[i];
+                    count = 1;
+                }
             }
         }
+        
         return result;
     }
 };
