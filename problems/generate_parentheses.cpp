aa2a23b1249e5445322ddbba9b161b1a884fbe52
Paolo Torres <paolo-torres@hotmail.com>
2022-01-22 00:25:40 -0800

    sp patterns 54/171

--- problems/generate_parentheses.cpp
+++ problems/generate_parentheses.cpp
@@ -1,21 +1,24 @@
+// Time: O(2^n)
+// Space: O(n)
+
 class Solution {
-private:
-    void generate(int left, int right, string str, vector<string>& result) {
-        if (left == 0 && right == 0) {
-            result.push_back(str);
-            return;
-        }
-        if (left > 0) {
-            generate(left - 1, right, str + '(', result);
-        }
-        if (left < right) {
-            generate(left, right - 1, str + ')', result);
-        }
-    }
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> result;
-        generate(n, n, "", result);
+        generate(n, 0, 0, "", result);
         return result;
+    }
+private:
+    void generate(int n, int open, int close, string str, vector<string>& result) {
+        if (open == n && close == n) {
+            result.push_back(str);
+            return;
+        }
+        if (open < n) {
+            generate(n, open + 1, close, str + '(', result);
+        }
+        if (open > close) {
+            generate(n, open, close + 1, str + ')', result);
+        }
     }
 };
