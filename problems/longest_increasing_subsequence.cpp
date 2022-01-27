807102c69c8a38a3d7ba2ee3ea18a34220a3e57d
Paolo Torres <paolo-torres@hotmail.com>
2022-01-26 23:04:58 -0800

    sp patterns 64/171

--- problems/longest_increasing_subsequence.cpp
+++ problems/longest_increasing_subsequence.cpp
@@ -4,23 +4,20 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        if (nums.empty()) {
-            return 0;
-        }
         int n = nums.size();
-        vector<int> dp(n, 0);
-        dp[0] = 1;
+        vector<int> dp(n, 1);
+        
         int result = 1;
+        
         for (int i = 1; i < n; i++) {
-            int maxValue = 0;
             for (int j = 0; j < i; j++) {
                 if (nums[j] < nums[i]) {
-                    maxValue = max(maxValue, dp[j]);
+                    dp[i] = max(dp[i], dp[j] + 1);
+                    result = max(result, dp[i]);
                 }
             }
-            dp[i] = maxValue + 1;
-            result = max(result, dp[i]);
         }
+        
         return result;
     }
 };
--- problems/longest_palindrome_substring.cpp
+++ problems/longest_palindrome_substring.cpp
@@ -4,25 +4,19 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
-        
-        if (n == 1) {
-            return s;
-        }
-        
         int maxStart = 0;
         int maxLength = 1;
         
-        for (int i = 0; i < n - 1; i++) {
-            expandPalindrome(s, i, i, maxStart, maxLength);
-            expandPalindrome(s, i, i + 1, maxStart, maxLength);
+        for (int i = 0; i < s.size() - 1; i++) {
+            middleOut(s, i, i, maxStart, maxLength);
+            middleOut(s, i, i + 1, maxStart, maxLength);
         }
         
         return s.substr(maxStart, maxLength);
     }
 private:
-    void expandPalindrome(string s, int i, int j, int& maxStart, int& maxLength) {
-        while (i >= 0 && j < s.size() && s[i] == s[j]) {
+    void middleOut(string s, int i, int j, int& maxStart, int& maxLength) {
+        while (i >= 0 && j <= s.size() - 1 && s[i] == s[j]) {
             i--;
             j++;
         }
