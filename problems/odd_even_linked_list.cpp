147d68191469215b918bb90b99680bbe060d3f4c
Paolo Torres <paolo-torres@hotmail.com>
2022-02-06 15:13:11 -0800

    sp patterns 89/171

--- problems/odd_even_linked_list.cpp
+++ problems/odd_even_linked_list.cpp
@@ -1,9 +1,14 @@
+// Time: O(n)
+// Space: O(1)
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
@@ -12,15 +17,18 @@ public:
         if (head == NULL || head->next == NULL) {
             return head;
         }
+        
         ListNode* odd = head;
         ListNode* even = head->next;
         ListNode* evenHead = even;
-        while (odd->next != NULL && even->next != NULL) {
-            odd->next = even->next;
+        
+        while (even != NULL && even->next != NULL) {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
             odd = odd->next;
-            even->next = odd->next;
             even = even->next;
         }
+        
         odd->next = evenHead;
         return head;
     }
