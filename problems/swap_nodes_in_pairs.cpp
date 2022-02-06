3c1434492bc8b8929d033b85b2a80bf02abb98af
Paolo Torres <paolo-torres@hotmail.com>
2022-02-06 12:53:40 -0800

    sp patterns 87/171

--- problems/swap_nodes_in_pairs.cpp
+++ problems/swap_nodes_in_pairs.cpp
@@ -1,9 +1,14 @@
+// Time: O(n)
+// Space: O(1)
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
@@ -12,29 +17,23 @@ public:
         if (head == NULL || head->next == NULL) {
             return head;
         }
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        ListNode* fix = head;
-        bool afterInit = false;
-        while (fast != NULL) {
-            ListNode* temp = fast->next;
-            fast->next = slow;
-            slow->next = temp;
-            if (afterInit) {
-                fix->next = fast;
-                fix = fix->next->next;
-            } else {
-                fix->next = temp;
-                head = fast;
-            }
-            slow = temp;
-            if (temp == NULL) {
-                fast = temp;
-            } else {
-                fast = temp->next;
-            }
-            afterInit = true;
+        
+        ListNode* dummy = new ListNode();
+        dummy->next = head;
+        
+        ListNode* curr = dummy;
+        
+        while (curr->next != NULL && curr->next->next != NULL) {
+            ListNode* p1 = curr->next;
+            ListNode* p2 = curr->next->next;
+            
+            curr->next = p2;
+            p1->next = p2->next;
+            
+            p2->next = p1;
+            curr = p1;
         }
-        return head;
+        
+        return dummy->next;
     }
 };
