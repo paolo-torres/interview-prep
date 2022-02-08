e61f7aa498f65144b871a122653f45414e5519c1
Paolo Torres <paolo-torres@hotmail.com>
2022-02-07 22:07:05 -0800

    sp patterns 95/171

--- problems/task_scheduler.cpp
+++ problems/task_scheduler.cpp
@@ -8,25 +8,25 @@ public:
     int leastInterval(vector<char>& tasks, int n) {
         vector<int> counter(26);
         
-        int maxValue = 0;
         int maxCount = 0;
+        int maxCountFrequency = 0;
         
         for (int i = 0; i < tasks.size(); i++) {
             counter[tasks[i] - 'A']++;
-            int currValue = counter[tasks[i] - 'A'];
+            int currCount = counter[tasks[i] - 'A'];
             
-            if (maxValue == currValue) {
-                maxCount++;
-            } else if (maxValue < currValue) {
-                maxValue = currValue;
-                maxCount = 1;
+            if (maxCount == currCount) {
+                maxCountFrequency++;
+            } else if (maxCount < currCount) {
+                maxCount = currCount;
+                maxCountFrequency = 1;
             }
         }
         
-        int partCount = maxValue - 1;
-        int partLength = n - (maxCount - 1);
+        int partCount = maxCount - 1;
+        int partLength = n - (maxCountFrequency - 1);
         int emptySlots = partCount * partLength;
-        int availableTasks = tasks.size() - maxValue * maxCount;
+        int availableTasks = tasks.size() - maxCount * maxCountFrequency;
         int idles = max(0, emptySlots - availableTasks);
         
         return tasks.size() + idles;
