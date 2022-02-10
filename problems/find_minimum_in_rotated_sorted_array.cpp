ab364a22741248e9f72b5899510784053e3e8628
Paolo Torres <paolo-torres@hotmail.com>
2022-02-09 21:29:27 -0800

    sp patterns 99/171

--- problems/find_minimum_in_rotated_sorted_array.cpp
+++ problems/find_minimum_in_rotated_sorted_array.cpp
@@ -8,13 +8,8 @@ public:
         int high = nums.size() - 1;
         
         while (low < high) {
-            if (nums[low] < nums[high]) {
-                return nums[low];
-            }
-            
             int mid = low + (high - low) / 2;
-            
-            if (nums[low] <= nums[mid]) {
+            if (nums[mid] > nums[high]) {
                 low = mid + 1;
             } else {
                 high = mid;
--- problems/find_peak_element.cpp
+++ problems/find_peak_element.cpp
@@ -1,11 +1,21 @@
+// Time: O(log n)
+// Space: O(1)
+
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        for (int i = 0; i < nums.size() - 1; i++) {
-            if (nums[i] > nums[i+1]) {
-                return i;
+        int low = 0;
+        int high = nums.size() - 1;
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid + 1;
             }
         }
-        return nums.size() - 1;
+        
+        return low;
     }
 };
