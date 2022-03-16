89503cbc3a0e8d67edb240c5419e729781433a21
Paolo Torres <paolo-torres@hotmail.com>
2022-03-16 15:47:37 -0700

    meta tagged

---
+++ leetcode/valid_palindrome_ii.cpp
@@ -0,0 +1,31 @@
+// Time: O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+        
+        while (i < j) {
+            if (s[i] != s[j]) {
+                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+private:
+    bool isPalindrome(string s, int i, int j) {
+        while (i < j) {
+            if (s[i] != s[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
