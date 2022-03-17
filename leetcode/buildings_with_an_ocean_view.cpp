76f40dcf5451b9923da1c866fafe07d72254a211
Paolo Torres <paolo-torres@hotmail.com>
2022-03-16 20:50:50 -0700

    meta tagged top 5

---
+++ leetcode/buildings_with_an_ocean_view.cpp
@@ -0,0 +1,20 @@
+// Time: O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    vector<int> findBuildings(vector<int>& heights) {
+        int maxHeight = 0;
+        vector<int> result;
+        
+        for (int i = heights.size() - 1; i >= 0; i--) {
+            if (heights[i] > maxHeight) {
+                result.push_back(i);
+                maxHeight = heights[i];
+            }
+        }
+        
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
