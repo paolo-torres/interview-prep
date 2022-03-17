e1a4c401e40dc2b2be929b5013810e658d94047a
Paolo Torres <paolo-torres@hotmail.com>
2022-03-16 23:19:12 -0700

    meta tagged top 7

---
+++ leetcode/lowest_common_ancestor_of_a_binary_tree_iii.cpp
@@ -0,0 +1,36 @@
+// Time: O(n)
+// Space: O(1)
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* parent;
+};
+*/
+
+class Solution {
+public:
+    Node* lowestCommonAncestor(Node* p, Node * q) {
+        Node* a = p;
+        Node* b = q;
+        
+        while (a != b) {
+            if (a == NULL) {
+                a = q;
+            } else {
+                a = a->parent;
+            }
+            if (b == NULL) {
+                b = p;
+            } else {
+                b = b->parent;
+            }
+        }
+        
+        return a;
+    }
+};
---
+++ leetcode/random_pick_with_weight.cpp
@@ -0,0 +1,45 @@
+// Time: O(n) Solution, O(log n) pickIndex
+// Space: O(n) Solution, O(1) pickIndex
+
+class Solution {
+public:
+    Solution(vector<int>& w) {
+        prefixSums = w;
+        int prefixSum = 0;
+        
+        for (int i = 0; i < w.size(); i++) {
+            prefixSum += w[i];
+            prefixSums[i] = prefixSum;
+        }
+        
+        totalSum = prefixSum;
+    }
+    
+    int pickIndex() {
+        double random = (double) rand() / RAND_MAX;
+        double target = random * totalSum;
+        
+        int low = 0;
+        int high = prefixSums.size() - 1;
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (target > prefixSums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        
+        return low;
+    }
+private:
+    vector<int> prefixSums;
+    int totalSum;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
